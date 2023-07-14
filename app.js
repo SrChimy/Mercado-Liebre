8d4356cdc4f54de75caeda5923c1b3fa7ac58667
SrChimy <ezebermejo97@gmail.com>
2023-07-13 23:02:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+const dotenv= require('dotenv').config();
+
+app.use(express.static("public"));
+app.get("/home", function (req, res){
+    let home = path.join(__dirname,"./views/home.html")
+    res.sendFile(home)
+});
+app.get("/login", function (req, res){
+    let login = path.join(__dirname,"./views/login.html")
+    res.sendFile(login)
+});
+app.get("/register", function (req, res){
+    let register = path.join(__dirname,"./views/register.html")
+    res.sendFile(register)
+});
+app.listen(process.env.PORT,() =>{
+    console.log ("Estamos corriendo")
+});
